ccfecdb47439bdd0d1c04b8288367a2b6189042b
nk <53134322+ninachka0107@users.noreply.github.com>
2024-10-18 16:15:23 +0900

    Update ch04.py

--- ch04.py
+++ ch04.py
@@ -22,7 +22,7 @@ print(avg_numbers(1,2,3,4,5))
 input1 = input("첫 번째 숫자를 입력하세요: ")
 input2 = input("두 번째 숫자를 입력하세요: ")
 
-total = int(input1) + int(input2)3
+total = int(input1) + int(input2)
 print("두 수의 합은 %s 입니다" % total)
 
 #4 출력 결과가 다른 것은?
